3697c8ed4e6b3af2b35f49a3f13ba72db1fcffc6
Ebrusike Favour <ebrusikefavour@gmail.com>
2026-06-19 11:42:31 +0100

    portfolio addition

--- assets/index.ts
+++ assets/index.ts
@@ -5,10 +5,31 @@ import Project003 from './photo_2026-06-19_02-38-27.jpg'
 import Project004 from './photo_2026-06-19_02-38-34.jpg'
 import Project005 from './photo_2026-06-19_02-38-40.jpg'
 import Project006 from './photo_2026-06-19_02-38-46.jpg'
+import Project007 from './photo_2026-06-19_09-22-28.jpg'
+import Project008 from './photo_2026-06-19_09-22-33.jpg'
+import Project009 from './photo_2026-06-19_09-22-39.jpg'
+import Project010 from './photo_2026-06-19_09-22-44.jpg'
+import Project011 from './photo_2026-06-19_09-22-49.jpg'
+import Project012 from './photo_2026-06-19_09-22-56.jpg'
+import Project013 from './photo_2026-06-19_09-23-01.jpg'
+import Project014 from './photo_2026-06-19_09-23-07.jpg'
+import Project015 from './photo_2026-06-19_09-23-11.jpg'
+import Project016 from './photo_2026-06-19_09-23-15.jpg'
+import Project017 from './photo_2026-06-19_09-23-20.jpg'
+import Project018 from './photo_2026-06-19_09-23-25.jpg'
+import Project019 from './photo_2026-06-19_09-23-30.jpg'
+import Project020 from './photo_2026-06-19_09-23-34.jpg'
+import Project021 from './photo_2026-06-19_09-23-40.jpg'
+import Project022 from './photo_2026-06-19_09-23-45.jpg'
+import Project023 from './photo_2026-06-19_11-30-54.jpg'
+import Project024 from './photo_2026-06-19_11-31-03.jpg'
 import Profilepng from './profile.png'
 
 
 export {
     Logo, Project001, Profilepng, Project002, Project003,
-    Project004, Project005, Project006
+    Project004, Project005, Project006, Project007, Project008,
+    Project009, Project010, Project011, Project012,Project013,Project014,
+    Project015,Project016,Project017,Project018,Project019,Project020,
+    Project021,Project022, Project023, Project024
 }
--- data/projects.ts
+++ data/projects.ts
@@ -1,4 +1,4 @@
-import { Project001, Project002, Project004, Project006 } from "@/assets";
+import { Project001, Project002, Project004, Project006, Project007, Project008, Project009, Project010, Project011, Project012, Project014, Project015, Project016, Project017, Project018, Project019, Project020, Project021, Project022, Project023, Project024 } from "@/assets";
 
 export const projects = [
   {
@@ -28,5 +28,89 @@ export const projects = [
     category: "Event Design",
     image: Project004
   },
- 
+    {
+    title: "Ladies Lash",
+    category: "Beauty industry",
+    image: Project010,
+  },
+  {
+    title: "Iperstock- Month of July",
+    category: "Finance Industry",
+    image: Project011
+  },
+  {
+    title: "JC Thistle- Month of July",
+    category: "Beverage Industry",
+    image: Project009
+  },
+  {
+    title: "Stay Offtrack - Shirt Mock up",
+    category: "Fashion Industry",
+    image: Project007,
+  },
+  {
+    title: "Dakodi Vogue - Month of July",
+    category: "Fashion Industry",
+    image: Project008,
+  },
+  {
+    title: "JC Thistle Roll up Stand",
+    category: "Brand Campaign",
+    image: Project022,
+  },
+  {
+    title: "Dakodi Vogue",
+    category: "Logo Design",
+    image: Project021,
+  },
+  {
+    title: "Signature by Ada - Women’s Day",
+    category: "Fashion industry",
+    image: Project020,
+  },
+  {
+    title: "Twinkle HealthCare - Post",
+    category: "Health Industry",
+    image: Project019,
+  },
+  {
+    title: "HerSpace Mental Wellness",
+    category: "Women Campaign",
+    image: Project018,
+  },
+  {
+    title: "JC Thistle - New Month",
+    category: "Beverage Marketing",
+    image: Project017,
+  },
+  {
+    title: "HerSpace Mental Wellness - Grand Opening",
+    category: "Mental Awareness Industry",
+    image: Project015,
+  },
+  {
+    title: "Flash Go Logistics",
+    category: "Transportation Industry",
+    image: Project014,
+  },
+  {
+    title: "Home Designers Associate - Mockup Logo",
+    category: "Real Estate Marketing",
+    image: Project016,
+  },
+  {
+    title: "Zivi’s Cakes - Beginners Class",
+    category: "Culinary Campaign",
+    image: Project012,
+  },
+  {
+    title: "Gidanka - Wifi ads",
+    category: "Real Estate Services",
+    image: Project024,
+  },
+  {
+    title: "Gidanka - Weekend Deals",
+    category: "Real Estate Campaign",
+    image: Project023,
+  },
 ];
